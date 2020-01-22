33d2fdb3307ee99e39d6e6a0d4e7e99a6cae9098
Kate Raskauskas <katherine.raskauskas@gmail.com>
2020-01-22 12:51:23 -0500

    put pseudocode and tty-prompt in run file

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,39 @@
 require_relative '../config/environment'
+prompt = TTY::Prompt.new
+
+# see all reviews for a user
+# see all reviews for bar
+
+# create review
+
+# create new bar/restaurant/place
+
+
+# user login stuff
+
+
+
+# start_action = prompt.select(' ',['Sign up','Log in'])
+# if start_action == 'Sign up'
+#     # create user
+# else
+#     # log in user
+# end
+
+# # create user method
+# def create_user
+
+# end
+# # log in method
+
+# see all reviews for logged in user (self)
 
 
 
 
-puts "HELLO WORLD"
+# edit review
+    # can only edit own reviews
+#delete review
+    # can only delete own reviews
+
+# add new bar method
