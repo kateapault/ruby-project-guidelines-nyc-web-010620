4b7c5987765d82f99c4a3f32a5e62bf05cea4c27
Kate Raskauskas <katherine.raskauskas@gmail.com>
2020-01-22 15:10:23 -0500

    fixed migration version

--- db/migrate/20200122140113_update_bars_columns.rb
+++ db/migrate/20200122140113_update_bars_columns.rb
@@ -1,4 +1,4 @@
-class UpdateBarsColumns < ActiveRecord::Migration[5.2]
+class UpdateBarsColumns < ActiveRecord::Migration[5.0]
   def change
     add_column :bars, :business_name, :string
     add_column :bars, :address_city, :string
