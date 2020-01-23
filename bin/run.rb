23652441a6308fdc29deeeea012e10eca60287a3
Eddie <mchh.004@gmail.com>
2020-01-23 10:43:21 -0500

    quick cleanup - reorganization - search by name

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,4 @@
 require_relative '../config/environment'
-
 prompt = TTY::Prompt.new
 
 def pause()
@@ -19,16 +18,15 @@ Y88b  d88P 888        Y88b. .d88P    888         888     888        888  T88b
                                                                              
                                                                               
 '
-
 puts logo
 logged_in = false
 until logged_in
     start_action = prompt.select('',['Log in','Sign up'])
     if start_action == 'Log in'
-        username = prompt.ask("Enter a username: ")
+        username = prompt.ask("Enter Username: ")
         user = User.all.find { |u| u.name == username }
         if user
-            pw = prompt.mask("Enter password: ")
+            pw = prompt.mask("Enter Password: ")
             if user.password == pw
                 logged_in = true
                 puts "Welcome, #{user.name}!"
@@ -36,7 +34,7 @@ until logged_in
                 puts "Wrong password! Please try again."
             end
         else
-            puts "No such user found. Please sign up."
+            puts "Username not found. Please sign up."
         end
     else
         # create user
@@ -49,68 +47,57 @@ end
 
 exit = false
 until exit 
+
     pause
-    action = prompt.select("What would you like to do?",["See my reviews","Change or delete a review","Search for a spot","Create review","Explore spots","Exit"])
+    action = prompt.select("What would you like to do?",
+        ["See My Reviews","Create Review","Change or Delete Review","Explore Spots","Exit"])
 
     case action 
-    when "See my reviews"
-        user.reviews
+
+    when "See My Reviews"
         puts "My Reviews:"
-        user.reviews.each do |r|
+        user.reload.reviews.each do |r|
             puts '-------------'
             spot = Bar.all.find { |b| b.id == r.bar_id }
-            puts "#{spot.business_name}"
-            puts "Your rating: #{r.rating}"
+            puts "#{spot.business_name} | #{spot.contact_number}"
+            puts "Rating: #{r.rating}"
         end
-    when "Change or delete a review"
-        choices = user.reviews.map do |r|
+
+    when "Create Review"
+        spot_name = prompt.ask("What Spot would you like to Review?")
+        spot = Bar.all.find { |b| b.business_name == spot_name }
+        rate = prompt.ask("Please rate #{spot_name} from 0-9")
+        new_review = Review.create({user_id: user.id,bar_id: spot.id,rating: rate})
+        puts "Review created!"
+
+    when "Change or Delete Review"
+        choices = user.reload.reviews.map do |r|
             spot = Bar.all.find { |b| b.id == r.bar_id }
             "#{spot.business_name}: #{r.rating}"
         end
-        my_review = prompt.enum_select("Choose a review to edit/delete:",choices)
+        my_review = prompt.enum_select("Choose a Review to Edit/Delete:",choices)
         ind = choices.find_index(my_review)
         review_to_change = user.reviews[ind]
-        crud = prompt.select("Would you like to edit or delete this review?",["Edit","Delete"])
+        crud = prompt.select("Would you like to Edit or Delete this Review?",["Edit","Delete"])
         case crud
         when 'Edit'
-            new_rating = prompt.ask("What is your new rating for this spot?")
+            new_rating = prompt.ask("What is your new Rating for this Spot?")
             review_to_change.rating = new_rating
             sleep(0.25)
-            puts "Your review has been changed!"
+            puts "Review Updated! "
         when 'Delete'
             x = prompt.yes?("Are you sure you want to delete this review?")
             if x
-                review_to_change.delete
+                review_to_change.reload.delete
+                puts(0.25)
+                puts "Review Deleted! (ノಠ益ಠ)ノ彡┻━┻"
             end
         end
-    when "Search for a spot"
-        spot_name = prompt.ask("Enter the name of the spot you want to search: ")
-        sleep(0.5)
-        puts "la la la searching for #{spot_name}"
-        sleep(0.5)
-        spot = Bar.all.find { |b| b.business_name == spot_name }
-        if spot
-            puts spot.business_name
-            rates = spot.reviews.map { |r| r.rating }
-            puts "Average rating: #{rates.sum/rates.size}"
-            puts "Reviews: "
-            spot.reviews.each do |review|
-                u = User.all.find { |user| user.id == review.user_id }
-                puts "#{u.name} gave this spot a #{review.rating}"
-            end
-        else
-            puts "No spot with that name found."
-        end
-    when "Create review"
-        spot_name = prompt.ask("What spot would you like to add a review for?")
-        spot = Bar.all.find { |b| b.business_name == spot_name }
-        rate = prompt.ask("How would you rate #{spot_name}?")
-        new_review = Review.create({user_id: user.id,bar_id: spot.id,rating: rate})
-        puts "Review created!"
-    when "Explore spots"
-        att = prompt.select('',['By zip code','By city','Random'])
+
+    when "Explore Spots"
+        att = prompt.select('',['By Name','By City','By Zip'])
         case att
-        when 'By zip code'
+        when 'By Zip'
             zip = prompt.ask("Enter zip code: ")
             zip_bars = Bar.all.select { |b| b.address_zip == zip }
             if zip_bars.size == 0
@@ -124,7 +111,8 @@ until exit
                     end
                 end
             end
-        when 'By city'
+
+        when 'By City'
             city = prompt.ask("Enter city: ")
             city_bars = Bar.all.select { |b| b.address_city == city }
             if !city_bars
@@ -138,48 +126,35 @@ until exit
                     end
                 end
             end
-        when 'Random'
-            Bar.all[0..5]
+        when 'By Name'
+        spot_name = prompt.ask("Enter the name of the spot you want to search: ")
+        sleep(0.5)
+        puts "la la la searching for #{spot_name}"
+        sleep(0.5)
+        spots = Bar.all.select do |b| 
+            if b["business_name"]
+                b["business_name"].include?(spot_name)
+                end
+            end
+        if spots.each do |t|
+            puts "#{t.business_name}, #{t.address_city}, #{t.contact_number}"
+            puts "---------------------------------"
+            rates = t.reviews.map { |r| r.rating }
+            real_rating = rates.each {|s| s > 0}
+            puts "Average rating: #{real_rating.sum/real_rating.size}"
+            puts "---------------------------------"
+            puts "Reviews: "
+            t.reviews.each do |review|
+                u = User.all.find { |user| user.id == review.user_id }
+                puts "#{u.name} gave this spot a #{review.rating}"
+            end
         end
+        else
+            puts "No spot with that name found."
+        end
+    end
     when "Exit"
         exit = true
     end
 
 end
-
-# create review
-def create_review(user)
-
-
-end
-
-# create new bar/restaurant/place
-
-
-# user login stuff
-
-
-# start_action = prompt.select(' ',['Sign up','Log in'])
-# if start_action == 'Sign up'
-#     # create user
-# else
-#     # log in user
-# end
-
-# # create user method
-# def create_user
-
-# end
-# # log in method
-
-# see all reviews for logged in user (self)
-
-
-
-
-# edit review
-    # can only edit own reviews
-#delete review
-    # can only delete own reviews
-
-# add new bar method
